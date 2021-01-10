2a38fbe8d10e676f0dede44f69853170ca3ed786
Cem Erdem Deniz <c-e-d@outlook.com.tr>
2021-01-10 23:27:00 +0000

    Initial Commit

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookingRadius = 10f;
+    Transform target;
+    public Transform player;
+    public NavMeshAgent agent;
+    public LayerMask whatIsGround;
+    public LayerMask whatIsPlayer;
+
+    public Vector3 walkingPoint;
+    bool walkingpointSet;
+    public float walkingRange;
+    public float betweenTimeAttack;
+    bool alreadyAttack;
+    public float enemyHealth;
+    public float sightRange;
+    public float attackRange;
+    public bool playerInSightRange;
+    public bool playerInattackRange;
+    Animator animm;
+    public GameObject projectTile;
+    Collider thisCol;
+
+
+
+
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        animm = GetComponent<Animator>();
+        thisCol = GetComponent<Collider>();
+    }
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+
+
+    }
+
+
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInattackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if (!playerInSightRange && !playerInattackRange)
+        {
+            animm.SetBool("LoafAround", true);
+            Patrolling();
+        }
+        if (playerInSightRange && !playerInattackRange)
+        {
+            animm.SetBool("Walking", true);
+            ChasePlayer();
+        }
+        if (playerInattackRange && playerInSightRange)
+        {
+            animm.SetTrigger("Attack");
+            AttackPlayer();
+        }
+
+    }
+
+
+
+
+    private void Patrolling()
+    {
+        if (!walkingpointSet)
+        {
+            SearchWalkingPoint();
+        }
+        if (walkingpointSet)
+        {
+
+            agent.SetDestination(walkingPoint);
+        }
+        Vector3 distanceToWalkPoint = transform.position - walkingPoint;
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkingpointSet = false;
+        }
+    }
+    private void SearchWalkingPoint()
+    {
+        float randomZ = Random.Range(-walkingRange, walkingRange);
+        float randomX = Random.Range(-walkingRange, walkingRange);
+        walkingPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if (Physics.Raycast(walkingPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkingpointSet = true;
+        }
+
+    }
+
+    private void ChasePlayer()
+    {
+
+        agent.SetDestination(player.position);
+        Debug.Log(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+        if (!alreadyAttack)
+        {
+
+            Rigidbody rigid = Instantiate(projectTile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rigid.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rigid.AddForce(transform.up * 8f, ForceMode.Impulse);
+            alreadyAttack = true;
+            Invoke(nameof(ResetAttack), betweenTimeAttack);
+
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttack = false;
+    }
+    public void TakeDamage(int damage)
+    {
+        enemyHealth -= damage;
+
+
+
+
+
+        if (enemyHealth <= 0)
+        {
+            animm.SetTrigger("Die");
+            animm.SetBool("AttackingDestroy", false);
+            //animm.GetComponent<Animator>().enabled = false;
+            Invoke(nameof(DestroyEnemy), 2f);
+        }
+    }
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "Player")
+        {
+
+        }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    Animator anim;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public Transform cam;
+    Rigidbody rigid;
+    GameObject enemyCol;
+
+    public float maxHealth = 100f;
+    public float cur_Health = 0f;
+
+
+
+    //public EnemyController enemyCont { get; set; }
+    public GameObject attackGameObject;
+
+
+
+
+
+
+    public float speed = 6f;
+    void Start()
+
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        attackGameObject = GetComponent<GameObject>();
+
+        cur_Health = maxHealth;
+        HealthBarSet();
+
+
+
+
+    }
+
+
+    void Update()
+    {
+
+        Attack();
+        Movement();
+
+
+    }
+
+    void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            anim.SetTrigger("Attack");
+
+
+        }
+
+
+
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetTrigger("Attack2");
+        }
+    }
+
+    void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (direction.magnitude >= 0.1f)
+        {
+            anim.SetInteger("Condition", 1);
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        else { anim.SetInteger("Condition", 0); }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //enemy health -10
+            //GetComponent<EnemyController>().TakeDamage(10);
+            Debug.Log("Enemy hit");
+        }
+    }
+
+
+    public void HealthBarSet()
+    {
+        float myHealth = cur_Health / maxHealth;
+    }
+}
